882775f1ad7fb2503c7ad0ab358e6d6aa45290e8
tow3rr <69804065+tow3rr@users.noreply.github.com>
2020-09-15 22:44:22 +0200

    Update spam.py

--- userbot/plugins/spam.py
+++ userbot/plugins/spam.py
@@ -22,15 +22,7 @@ async def spammer(e):
             )
 
 
-@register(outgoing=True, pattern="^.tspam")
-async def tmeme(e):
-    tspam = str(e.text[7:])
-    message = tspam.replace(" ", "")
-    for letter in message:
-        await e.respond(letter)
-    await e.delete()
-        
-        @register(outgoing=True, pattern="^.bigspam")
+@register(outgoing=True, pattern="^.bigspam")
 async def bigspam(e):
     if not e.text[0].isalpha() and e.text[0] not in ("/", "#", "@", "!"):
         message = e.text
