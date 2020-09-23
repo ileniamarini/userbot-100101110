c66e406a698948133be50f28192a60b653e03fcd
tow3rr <69804065+tow3rr@users.noreply.github.com>
2020-09-23 18:41:01 +0200

    Create spam2.py

---
+++ userbot/plugins/spam2.py
@@ -0,0 +1,15 @@
+import asyncio
+import time
+
+from asyncio import wait, sleep
+from telethon import events
+from userbot import BOTLOG, BOTLOG_CHATID, CMD_HELP
+from userbot.system import register
+
+@register(outgoing=True, pattern="^.spam (.*)")
+async def tmeme(e):
+     tspam = str(e.text[7:])
+     message = tspam.replace(" ", "")
+     for letter in message:
+         await e.respond(letter)
+     await e.delete()
