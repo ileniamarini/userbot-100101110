baeaace6579682d71c62c028c5bc99d7483f859e
tow3rr <69804065+tow3rr@users.noreply.github.com>
2020-09-23 20:00:16 +0200

    Update _helper.py

--- userbot/plugins/_helper.py
+++ userbot/plugins/_helper.py
@@ -43,7 +43,7 @@ async def cmd_list(event):
             else:
                 await event.edit(input_str + " plugin non valido!")
         else:
-            help_string = f"""Userbot Helper.. di {DEFAULTUSER}\nEditato da @CoIIassato❤️📶\n
+            help_string = f"""Userbot Helper.. di {DEFAULTUSER}\nCrediti: Sto nell'anonimo\n
 `Helper per i comandi`\nPremi .help plugin_name se non è qui."""
             results = await bot.inline_query(  # pylint:disable=E0602
                 tgbotusername,
